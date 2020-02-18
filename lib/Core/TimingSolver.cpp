2c159f7972bb43cebeaf662dc93d8f4b2eccf890
David Trabish <davidtr1037@gmail.com>
2020-02-18 18:30:54 +0200

    updating

--- lib/Core/TimingSolver.cpp
+++ lib/Core/TimingSolver.cpp
@@ -20,10 +20,70 @@
 using namespace klee;
 using namespace llvm;
 
+cl::opt<bool> CollectQueryStats("collect-query-stats", cl::init(true), cl::desc("..."));
+
 /***/
 
+bool SolverQuery::isEqual(const SolverQuery &other) const {
+  if (constraints.size() != other.constraints.size()) {
+    return false;
+  }
+
+  if (expr->compare(*other.expr) != 0) {
+    return false;
+  }
+
+  auto iter1 = constraints.begin();
+  auto iter2 = other.constraints.begin();
+  while (iter1 != constraints.end()) {
+    ref<Expr> e1 = *iter1;
+    ref<Expr> e2 = *iter2;
+    if (e1->compare(*e2) != 0) {
+      return false;
+    }
+
+    iter1++; iter2++;
+  }
+  return true;
+}
+
+bool SolverQuery::isIsomorphic(const SolverQuery &other) const {
+  if (constraints.size() != other.constraints.size()) {
+    return false;
+  }
+
+  if (!expr->isIsomorphic(*other.expr)) {
+    return false;
+  }
+
+  auto iter1 = constraints.begin();
+  auto iter2 = other.constraints.begin();
+  while (iter1 != constraints.end()) {
+    ref<Expr> e1 = *iter1;
+    ref<Expr> e2 = *iter2;
+    if (!e1->isIsomorphic(*e2)) {
+      return false;
+    }
+
+    iter1++; iter2++;
+  }
+  return true;
+}
+
+void SolverQuery::dump() const {
+  llvm::errs() << "Constraints [\n";
+  for (ConstraintManager::const_iterator i = constraints.begin();
+      i != constraints.end(); i++) {
+    (*i)->dump();
+  }
+  llvm::errs() << "]\n";
+  llvm::errs() << "Query [\n";
+  expr->dump();
+  llvm::errs() << "]\n";
+}
 bool TimingSolver::evaluate(const ExecutionState& state, ref<Expr> expr,
                             Solver::Validity &result) {
+  handleExpr(state, expr);
   expr = state.unfold(expr);
   // Fast path, to avoid timer and OS overhead.
   if (ConstantExpr *CE = dyn_cast<ConstantExpr>(expr)) {
@@ -49,6 +109,7 @@ bool TimingSolver::evaluate(const ExecutionState& state, ref<Expr> expr,
 
 bool TimingSolver::mustBeTrue(const ExecutionState& state, ref<Expr> expr, 
                               bool &result) {
+  handleExpr(state, expr);
   expr = state.unfold(expr);
   // Fast path, to avoid timer and OS overhead.
   if (ConstantExpr *CE = dyn_cast<ConstantExpr>(expr)) {
@@ -97,6 +158,7 @@ bool TimingSolver::mayBeFalse(const ExecutionState& state, ref<Expr> expr,
 
 bool TimingSolver::getValue(const ExecutionState& state, ref<Expr> expr, 
                             ref<ConstantExpr> &result) {
+  handleExpr(state, expr);
   expr = state.unfold(expr);
   // Fast path, to avoid timer and OS overhead.
   if (ConstantExpr *CE = dyn_cast<ConstantExpr>(expr)) {
@@ -168,6 +230,10 @@ void TimingSolver::fillConstraints(const ExecutionState &state,
 }
 
 void TimingSolver::handleExpr(const ExecutionState &state, ref<Expr> expr) {
+  if (!CollectQueryStats) {
+    return;
+  }
+
   if (!expr->flag) {
     return;
   }
@@ -177,10 +243,15 @@ void TimingSolver::handleExpr(const ExecutionState &state, ref<Expr> expr) {
     return;
   }
 
-  std::vector<ref<Expr>> pc(state.constraints.begin(), state.constraints.end());
-  SolverQuery q(pc, expr);
+  /* slice the path constraints */
+  Query query(state.constraints, expr);
+  std::vector<ref<Expr>> required;
+  sliceConstraints(query, required);
+
+  //std::vector<ref<Expr>> pc(state.constraints.begin(), state.constraints.end());
+  //SolverQuery q(pc, expr);
+  SolverQuery q(required, expr);
   queries_count++;
-  //q.dump();
 
   bool found;
 
